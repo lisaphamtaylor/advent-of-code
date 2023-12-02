3304f4153b09308cdaf9bfed9c7cc2871051b40e
Lisa <113562541+lisaphamtaylor@users.noreply.github.com>
2023-12-01 19:20:12 -0800

    divvy out functions in part 1 and sets up part 2

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,5 +1,6 @@
 """
 --- Day 1: Trebuchet?! ---
+https://adventofcode.com/2023/day/1
 Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
 You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
@@ -27,27 +28,80 @@ Consider your entire calibration document. What is the sum of all of the calibra
 # opening file and reading lines of data and saving to list
 data = open('day1_input.txt').read().split('\n')
 
-def sum_first_last(dataset):
-    '''returns sum of first and last digit of each line in dataset'''
+def sum_first_last_digit(line):
+    '''returns sum of first and last numeric digit in input line'''
+    # initializing line_num str to hold all line digits
+    line_num = ''
+    # iterating over each character in line
+    for char in line:
+        # if character is digit, add to line_num
+        if char.isdigit():
+            line_num += char
+        
+    # return 2 digit int of first and last digit in line_num
+    return int(line_num[0]+line_num[-1])
+
+def sum_data_lines(dataset):
+    '''returns sum of first and last numeric digit of each line in dataset'''
     # initializing sum
     sum = 0
 
     # iterating over each line of data
-    for line in data:
-        # checking if line is populated
-        if line:
-            # initializing line_num str to hold all line digits
-            line_num = ''
-            # iterating over each character in line
-            for char in line:
-                # if character is digit, add to line_num
-                if char.isdigit():
-                    line_num += char
+    for line in dataset:
+        # run sum_first_last_digit in each line and add value to sum
+        sum += sum_first_last_digit(line)
         
-        # adding first and last digit of line_num to sum
-        sum += int(line_num[0])*10+int(line_num[-1])
-    print(sum)
+    return sum
 
-sum_first_last(data) # sum was 54708
+print(f'Part 1 answer: {sum_data_lines(data)}')     # sum = 54708
 
+"""--- Part Two ---
+https://adventofcode.com/2023/day/1#part2
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?"""
 # --- Part Two ---
+# create digit dictionary to translate digit words into ints
+digit_dict = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4', 
+    'five': '5',
+    'six': '6',
+    'seven': '7', 
+    'eight': '8',
+    'nine': '9'
+}
+
+def sum_first_last_w_words(dataset):
+    '''returns sum of first and last digit--word or int---of each line in dataset'''
+    # initializing sum
+    total_sum = 0
+    
+    # iterating over each line of data
+    for line in dataset:
+        # check to see if line contains any digit words
+        while any(word_digit in line for word_digit in digit_dict.keys()):
+            # if word_digit, replace with string of int
+            for word_digit in digit_dict.keys():
+                if word_digit in line:
+                    line = line.replace(word_digit, digit_dict[word_digit])
+        line_sum = sum_first_last_digit(line)
+        total_sum += line_sum
+        print(line, sum_first_last_digit(line), total_sum)
+    
+    return total_sum
+
+print(f'Part 2 answer: {sum_first_last_w_words(data)}')
